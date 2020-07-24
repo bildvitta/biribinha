dd7992a0aeb00fde493e8c894072b9816c38db35
Pedro Pizzo de Paula <poloo.p3@gmail.com>
2020-07-24 17:17:44 -0300

    hint add

--- src/form/label.js
+++ src/form/label.js
@@ -1,14 +1,34 @@
+import { createPopper } from '@popperjs/core';
+import './style.scss';
+
 function createInputLabel({ label, name, hint }) {
   const labelDiv = document.createElement('label');
   labelDiv.setAttribute('for', name); // Create Label for Name Field
   labelDiv.innerHTML = `${label}:`; // Set Field Labels
   if (hint) {
     const hintDiv = document.createElement('span');
-    hintDiv.setAttribute('class', 'material-icons');
+    hintDiv.classList.add('with-tooltip');
+    hintDiv.classList.add('material-icons');
     hintDiv.innerHTML = 'help';
-    hintDiv.setAttribute('data-toggle', 'tooltip');
-    hintDiv.setAttribute('data-placement', 'top');
-    hintDiv.setAttribute('title', hint);
+    const tooltip = document.createElement('div');
+    tooltip.setAttribute('class', 'tooltip');
+    tooltip.setAttribute('role', 'tooltip');
+    tooltip.innerHTML = `${hint}`;
+
+    hintDiv.appendChild(tooltip);
+
+    createPopper(hintDiv, tooltip, {
+      placement: 'top',
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
+        },
+      ],
+    });
+
     labelDiv.append(hintDiv);
   }
   return labelDiv;
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,43 @@
-const path = require("path");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: './src/app.js',
   output: {
-    publicPath: "/dist/",
-    path: path.resolve("dist"),
-    filename: "bundle.js",
+    publicPath: '/dist/',
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         exclude: /\.module\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       {
         test: /\.module\.css$/,
         use: [
-          { loader: "style-loader" },
-          { loader: "css-loader", options: { modules: true, camelCase: true } },
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { modules: true, camelCase: true } },
         ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
-        use: "file-loader",
+        use: 'file-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
       },
     ],
   },
