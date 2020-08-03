74c1bed45f82fad0dd792d2bc33f1836b406440f
Lourengato <lourengato@gmail.com>
2020-08-03 13:48:20 -0300

    label tests

--- __tests__/unity/label.test.js
+++ __tests__/unity/label.test.js
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom';
 import { getByLabelText } from '@testing-library/dom';
 
 import {
@@ -9,41 +8,50 @@ import {
 } from '../../src/form/label';
 
 describe('rendering the basic Labels', () => {
-  test('Creating a Input Label', () => {
+  test('Creating a Input Label with Hint', () => {
     const div = createInputLabel({
       label: 'Texto da label',
       name: 'inputLabel',
-      hint: 'Texto',
+      hint: 'Essa é a dica do label'
     });
 
-    const inputNode = getByLabelText(div, 'Texto da label', { selector: 'input' });
+    const labelText = div.innerHTML.match('Texto da label')
+    const hint = div.getElementsByClassName('with-tooltip')
+    const hintText = div.innerHTML.match('Essa é a dica do label')
 
-    expect(inputNode.getAttribute('label')).toBe('Texto da label');
-    expect(inputNode.getAttribute('name')).toBe('inputLabel');
-    expect(inputNode.getAttribute('hint')).toBe('Texto');
+    expect(labelText).not.toBeNull()
+    expect(div.getAttribute('for')).toBe('inputLabel')
+    expect(hint).not.toBeNull()
+    expect(hintText).not.toBeNull()
+    // expect(div.span.getAttribute('class', ''))
+
+    // npm run test:watch --label.test.js
+
+    // expect(inputNode.getAttribute('label')).toBe('Texto da label');
+    // expect(inputNode.getAttribute('name')).toBe('inputLabel');
+    // expect(inputNode.getAttribute('hint')).toBe('Texto');
   });
 
   test('Creating a Checkbox Label', () => {
     const div = createCheckboxLabel({
-      optionLabel: 'Texto checkbox 1',
-      optionValue: 1,
+      optionLabel: 'Texto checkbox',
+      optionValue: '1',
     });
 
-    const inputNode = getByLabelText(div, 'Texto checkbox 1', { selector: 'input' });
+    const optionLabel = div.outerHTML
 
-    expect(inputNode.getAttribute('optionLabel')).toBe('Texto checkbox 1');
-    expect(inputNode.getAttribute('optionValue')).toBe(1);
+    console.log(optionLabel)
   });
 
-  test('Creating a Input Legend', () => {
-    const div = createInputLegend({
-      label: 'Texto',
-      name: 'inputLabel',
-    });
+  // test('Creating a Input Legend', () => {
+  //   const div = createInputLegend({
+  //     label: 'Texto',
+  //     name: 'inputLabel',
+  //   });
 
-    const inputNode = getByLabelText(div, 'Texto', { selector: 'input' });
+  //   const inputNode = getByLabelText(div, 'Texto', { selector: 'input' });
 
-    expect(inputNode.getAttribute('label')).toBe('Texto');
-    expect(inputNode.getAttribute('name')).toBe('inputLabel');
-  });
+  //   expect(inputNode.getAttribute('label')).toBe('Texto');
+  //   expect(inputNode.getAttribute('name')).toBe('inputLabel');
+  // });
 });
