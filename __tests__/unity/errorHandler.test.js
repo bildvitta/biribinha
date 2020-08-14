a085e9e427396130900ea77ba521131016e1bcef
Lourengato <lourengato@gmail.com>
2020-08-14 14:54:54 -0300

    error handler test

--- __tests__/unity/errorHandler.test.js
+++ __tests__/unity/errorHandler.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import errorHandler from '../../src/utils/errorHandler';
 
-test('error handling', () => {
+test('error handling with single error', () => {
   document.body.innerHTML = `
     <div id="app">
       <div class="form-group row">
@@ -26,5 +26,34 @@ test('error handling', () => {
 
   expect(
     errorsResponse[0].innerHTML.includes('Campo obrigatório')
+  ).toBeTruthy();
+});
+
+test('error handling with multiple errors', () => {
+  document.body.innerHTML = `
+    <div id="app">
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label" for="name">
+          Nome
+        </label>
+        <div class="col-sm-9">
+          <input name="name" id="name" class="form-control is-invalid" type="text" readonly="true" value="Pedro Silva de Imbondo">
+        </div>
+      </div>
+    </div>
+  `;
+  const errors = {
+    name: [
+      'Campo obrigatório',
+      'Deve ter um nome válido'
+    ],
+  };
+
+  errorHandler(errors);
+
+  const errorsResponse = document.getElementsByClassName('invalid-feedback');
+
+  expect(
+    errorsResponse[0].innerHTML.includes('Campo obrigatório<br>Deve ter um nome válido')
   ).toBeTruthy();
 });
